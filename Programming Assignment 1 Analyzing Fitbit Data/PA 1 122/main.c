d9e44241578b4e16e37c6e81c73e60a805be0e40
ArmitageG <72293385+ArmitageG@users.noreply.github.com>
2021-11-24 21:09:06 -0800

    Add files via upload

---
+++ Programming Assignment 1 Analyzing Fitbit Data/PA 1 122/main.c
@@ -0,0 +1,21 @@
+/*******************************************************************************************
+
+* Programmer: Anthony Ghimpu                                                                       *
+
+* Class: CptS 122, Spring 2021; Lab Section 9                                            *
+
+* Programming Assignment: PA 1                                                     *
+
+* Date: January 25, 2020                                                                           *
+
+* Description: This program parses, cleans, and releases information on the fitbit data         *
+
+*******************************************************************************************/
+#include "header.h"
+int main(void) {
+	
+
+	
+	readParseClenseData();
+	return 0;
+}
